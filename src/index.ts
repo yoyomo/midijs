d65c7e4d133a7b672e4d8107d4b3893e7b883a10
Armando Ortiz <armando@solairo.co.jp>
2019-12-09 19:07:00 +0900

    wip events and delta time

--- src/index.ts
+++ src/index.ts
@@ -23,16 +23,30 @@ const stringToBytes = (midiString: string): Uint8Array => {
   return u8;
 };
 
-const getNBytes = (midi: Uint8Array, c: number, n: number): number => {
+const getNBytes = (midi: Uint8Array, start: number, n: number): number => {
   let nBytes = 0;
   for (let b = 0; b < n; b++) {
-    const byte = midi[c];
+    const byte = midi[start + b];
     nBytes = nBytes << (8 * b) + byte;
   }
   return nBytes;
 };
 
+const getDeltaTime = (midi: Uint8Array, start: number) => {
+  let deltaTime = 0;
+  let deltaTimeNBytes = 0;
+  let deltaTimeByte;
+  do{
+    deltaTimeByte = midi[start + deltaTimeNBytes];
+    deltaTime = (deltaTime << 7) + deltaTimeByte & 0x7F;
+    deltaTimeNBytes++;
+  } while(deltaTimeByte & 0x80);
+
+  return deltaTime;
+};
+
 type MidiTypes = "MThd" | "MTrk";
+type DivisionTypes = "metrical" | "time-code-based";
 
 const parseMIDIBytes = (midi: Uint8Array) => {
 
@@ -40,7 +54,7 @@ const parseMIDIBytes = (midi: Uint8Array) => {
     format: 0,
     tracks: 0,
     division: {
-      type: 0,
+      type: "metrical" as DivisionTypes,
       ticksPerQuarterNote: 0,
       ticksPerFrame: 0,
       framesPerSecond: 0
@@ -56,15 +70,24 @@ const parseMIDIBytes = (midi: Uint8Array) => {
 
     switch (type) {
       case "MThd":
-        header.format = getNBytes(midi, c, 2);
-        header.tracks = getNBytes(midi, c + 2, 2);
-        const division = getNBytes(midi, c + 4, 2);
+        const twoBytes = 2;
+        let headerBytePosition = c;
+        const format = getNBytes(midi, headerBytePosition, twoBytes);
+        const tracks = getNBytes(midi, headerBytePosition += twoBytes, twoBytes);
+        const division = getNBytes(midi, headerBytePosition += twoBytes, twoBytes);
 
-        header.division.type = division & 0x8000;
+        header.format = format;
+        header.tracks = tracks;
+        header.division.type = division & 0x8000 ? "time-code-based" : "metrical";
 
-        if(header.division.type) {
+        if(header.division.type === "time-code-based") {
           header.division.ticksPerFrame = division & 0x00FF;
-          header.division.framesPerSecond = (division & 0x7F00) >> 8;
+
+          const bit8_15 = (division & 0xFF00) >> 8;
+          const flippedBit8_15 = bit8_15 ^ 0xFF;
+          const addOneFlippedBit8_15 = flippedBit8_15 + 1;
+
+          header.division.framesPerSecond = addOneFlippedBit8_15;
         } else {
           header.division.ticksPerQuarterNote = division & 0x7FFF;
         }
@@ -74,6 +97,16 @@ const parseMIDIBytes = (midi: Uint8Array) => {
         break;
       case "MTrk":
         // TODO handle Track
+
+        const deltaTime = getDeltaTime(midi, c);
+        let event;
+
+
+
+
+        // TODO save track object
+
+        // TODO fire TrackEvent
 
 
 
